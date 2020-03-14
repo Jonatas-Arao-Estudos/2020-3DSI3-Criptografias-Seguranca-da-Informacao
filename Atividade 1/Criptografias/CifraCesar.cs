e38d845ffed1614e4a612761ade7a37712c98a8d
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-03-14 18:00:31 -0300

    Atividade 1 - Cifra de César Completa

---
+++ Atividade 1/Criptografias/CifraCesar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografias
+{
+    class CifraCesar : Palavra
+    {
+        private int chave;
+
+        public CifraCesar()
+            : base()
+        {
+            this.chave = 0;
+        }
+
+        public CifraCesar(string palavra, int chave)
+            : base(palavra)
+        {
+            this.chave = chave;
+        }
+
+        public int Chave
+        {
+            get { return chave; }
+            set { chave = value; }
+        }
+
+        public string Criptografar()
+        {
+            ASCIIEncoding ASCII = new ASCIIEncoding();
+            byte[] palavraBytes = this.PalavraBytes();
+            for (int i = 0; i < palavraBytes.Length; i++)
+            {
+                if (palavraBytes[i] >= 65 && palavraBytes[i] <= 90)
+                {
+                    palavraBytes[i] = calculaCifraCesar(65, this.chave, palavraBytes[i]);
+                }
+                if (palavraBytes[i] >= 97 && palavraBytes[i] <= 122)
+                {
+                    palavraBytes[i] = calculaCifraCesar(97, this.chave, palavraBytes[i]);
+                }
+            }
+
+            byte calculaCifraCesar(int inicio, int chave , byte letra)
+            {
+                int salto = inicio + ((chave + (letra - inicio)) % 26);
+                letra = Convert.ToByte(salto);
+                return letra;
+            }
+
+            string palavraCodificada = ASCII.GetString(palavraBytes);
+            return palavraCodificada;
+        }
+    }
+}
---
+++ Atividade 1/Criptografias/Criptografia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografias
+{
+    class Palavra
+    {
+        private string palavraEscolhida;
+
+        public Palavra()
+        {
+            this.palavraEscolhida = null;
+        }
+        public Palavra(string palavraEscolhida)
+        {
+            this.palavraEscolhida = palavraEscolhida;
+        }
+
+        public string PalavraEscolhida
+        {
+            get { return palavraEscolhida; }
+            set {
+                palavraEscolhida = value;
+            }
+        }
+
+        public byte[] PalavraBytes()
+        {
+            ASCIIEncoding ASCII = new ASCIIEncoding();
+            byte[] palavraBytes = ASCII.GetBytes(this.palavraEscolhida);
+            return palavraBytes;
+        }
+    }
+}
